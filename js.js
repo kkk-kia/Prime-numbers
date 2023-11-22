1d55dac4214c933bfdf3a0ae9a0ce0e08fa1989a
kourOsh <kouroshkia95@gmail.com>
2023-11-22 19:25:24 +0330

    Increased calculation (up to 1 million tested) and creat pixel

--- js.js
+++ js.js
@@ -5,14 +5,15 @@ var answer = document.getElementById("answer");
 var divPixel = document.getElementById("divPixel")
 var divInput = document.getElementById("divInput")
 
+var avalNumber = []
 
 btn.addEventListener("click", giveToUser);
 
 
 function giveToUser() {
   answer.innerHTML = ""
+  avalNumber = [];
 
-  var avalNumber = [];
   if (from.value <= 2) {
     avalNumber.push(2)
   }
@@ -23,19 +24,45 @@ function giveToUser() {
   for (var i = 1; i < to.value; i++) {
     var x = (6 * i) - 1
     var y = (6 * i) + 1
-    if (x >= from.value && x <= to.value) {
-      avalNumber.push(x)
-    }
+
     if (y >= from.value && y <= to.value) {
+      avalNumber.push(x)
       avalNumber.push(y)
-    }
-    if (x > to.value) {
+    } else {
+      if (x >= from.value && y <= to.value) {
+        avalNumber.push(x)
+      }
       break
     }
   }
 
 
+  // for (i = 0; i < avalNumber.length; i++) {
+  //   answer.innerHTML += avalNumber[i] + ", "
+  // }
+
+  pixelMade()
+}
+
+
+function pixelMade() {
+  divInput.style.display = "none"
+  divPixel.style.gridTemplateColumns = "repeat" + "(" + window.innerWidth + ",1px)"
+  divPixel.style.gridTemplateRows = "repeat" + "(" + window.innerHeight + ",1px)"
+
   for (i = 0; i < avalNumber.length; i++) {
-    answer.innerHTML += avalNumber[i] + ", "
+    var pixel = document.createElement("div")
+    var row = 1
+    var column = avalNumber[i]
+    while (column > window.innerWidth) {
+      column -= window.innerWidth
+      row += 1
+    }
+    divPixel.appendChild(pixel)
+    pixel.style.width = "1px"
+    pixel.style.height = "1px"
+    pixel.style.gridColumnStart = column
+    pixel.style.gridRowStart = row
+    pixel.style.backgroundColor = "red"
   }
 }
